23e6318790001dbbbb8a708f5822f3f7c35e4ef4
wangziyue <Ziyue.Wang@lavainternational.com>
2017-02-27 15:19:54 +0800

    Update music info by AsyncTask.

--- app/src/main/java/randommusic/wzy/self/random/MainActivity.java
+++ app/src/main/java/randommusic/wzy/self/random/MainActivity.java
@@ -2,6 +2,7 @@ package randommusic.wzy.self.random;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.os.AsyncTask;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -21,22 +22,22 @@ public class MainActivity extends AppCompatActivity {
     public static int PAUSE_MSG = 1;
     public static int STOP_MSG = 2;
     private ListView mMusiclist = null;
-    private List<MusicInfo> infos = null;
+    private List<MusicInfo> infosList = null;
     private SimpleAdapter mAdapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        infos = getAllMusicInfos();
         mMusiclist = (ListView)findViewById(R.id.list_layout);
-        setListAdpter(infos);
+        UpdateInfoTask mUpdateInfoTask = new UpdateInfoTask();
+        mUpdateInfoTask.execute();
         mMusiclist.setOnItemClickListener(new MusicListItemClickListener());
 
-        Button btn1 = (Button)findViewById(R.id.ctrl_pause);
-        Button btn2 = (Button)findViewById(R.id.ctrl_stop);
+        Button pauseBtn = (Button)findViewById(R.id.ctrl_pause);
+        Button stopBtn = (Button)findViewById(R.id.ctrl_stop);
 
-        btn1.setOnClickListener(new View.OnClickListener() {
+        pauseBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent();
@@ -46,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        btn2.setOnClickListener(new View.OnClickListener() {
+        stopBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent();
@@ -57,43 +58,6 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
-
-
-    public List<MusicInfo> getAllMusicInfos() {
-        Cursor cursor = getContentResolver().query(
-                MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, null, null, null,
-                MediaStore.Audio.Media.DEFAULT_SORT_ORDER);
-        List<MusicInfo> MusicInfos = new ArrayList<MusicInfo>();
-        for (int i = 0; i < cursor.getCount(); i++) {
-            MusicInfo MusicInfo = new MusicInfo();
-            cursor.moveToNext();
-            long id = cursor.getLong(cursor
-                    .getColumnIndex(MediaStore.Audio.Media._ID));
-            String title = cursor.getString((cursor
-                    .getColumnIndex(MediaStore.Audio.Media.TITLE)));
-            String artist = cursor.getString(cursor
-                    .getColumnIndex(MediaStore.Audio.Media.ARTIST));
-            long duration = cursor.getLong(cursor
-                    .getColumnIndex(MediaStore.Audio.Media.DURATION));
-            long size = cursor.getLong(cursor
-                    .getColumnIndex(MediaStore.Audio.Media.SIZE));
-            String url = cursor.getString(cursor
-                    .getColumnIndex(MediaStore.Audio.Media.DATA));
-            int isMusic = cursor.getInt(cursor
-                    .getColumnIndex(MediaStore.Audio.Media.IS_MUSIC));
-            if (isMusic != 0) {
-                MusicInfo.setId(id);
-                MusicInfo.setTitle(title);
-                MusicInfo.setArtist(artist);
-                MusicInfo.setDuration(duration);
-                MusicInfo.setSize(size);
-                MusicInfo.setUrl(url);
-                MusicInfos.add(MusicInfo);
-            }
-        }
-        return MusicInfos;
-    }
-
 
     public void setListAdpter(List<MusicInfo> allInfos) {
         List<HashMap<String, String>> mp3list = new ArrayList<HashMap<String, String>>();
@@ -113,13 +77,61 @@ public class MainActivity extends AppCompatActivity {
         mMusiclist.setAdapter(mAdapter);
     }
 
+    private class UpdateInfoTask extends AsyncTask<Integer, Integer, List<MusicInfo>>{
+        @Override
+        protected List<MusicInfo> doInBackground(Integer... num) {
+            List<MusicInfo> MusicInfos = new ArrayList<MusicInfo>();
+            Cursor cursor = getContentResolver().query(
+                    MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, null, null, null,
+                    MediaStore.Audio.Media.DEFAULT_SORT_ORDER);
+            for (int i = 0; i < cursor.getCount(); i++) {
+                MusicInfo MusicInfo = new MusicInfo();
+                cursor.moveToNext();
+                long id = cursor.getLong(cursor
+                        .getColumnIndex(MediaStore.Audio.Media._ID));
+                String title = cursor.getString((cursor
+                        .getColumnIndex(MediaStore.Audio.Media.TITLE)));
+                String artist = cursor.getString(cursor
+                        .getColumnIndex(MediaStore.Audio.Media.ARTIST));
+                long duration = cursor.getLong(cursor
+                        .getColumnIndex(MediaStore.Audio.Media.DURATION));
+                long size = cursor.getLong(cursor
+                        .getColumnIndex(MediaStore.Audio.Media.SIZE));
+                String url = cursor.getString(cursor
+                        .getColumnIndex(MediaStore.Audio.Media.DATA));
+                int isMusic = cursor.getInt(cursor
+                        .getColumnIndex(MediaStore.Audio.Media.IS_MUSIC));
+                if (isMusic != 0) {
+                    MusicInfo.setId(id);
+                    MusicInfo.setTitle(title);
+                    MusicInfo.setArtist(artist);
+                    MusicInfo.setDuration(duration);
+                    MusicInfo.setSize(size);
+                    MusicInfo.setUrl(url);
+                    MusicInfos.add(MusicInfo);
+                }
+            }
+            return MusicInfos;
+        }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+        }
+
+        @Override
+        protected void onPostExecute(List<MusicInfo> list) {
+            infosList = list;
+            setListAdpter(list);
+        }
+    }
 
     private class MusicListItemClickListener implements AdapterView.OnItemClickListener {
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position,
                                 long id) {
-            if(infos != null) {
-                MusicInfo info = infos.get(position);
+            if(infosList != null) {
+                MusicInfo info = infosList.get(position);
                 Intent intent = new Intent();
                 intent.putExtra("url", info.getUrl());
                 android.util.Log.w("----------",info.getTitle());
